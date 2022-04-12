be0f5fb1fe83ced78168177935bd4997ed8460bb
Ricardo Targino <64715326+RickHub2002@users.noreply.github.com>
2022-04-12 18:04:03 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
 const url = 'https://api.github.com/users/RickHub2002';
+//Se quiser alterar para a sua foto de usuário do github é só colocar no lugar do "RickHub2002" o seu nome de usuário.
+
 
 const getUrl = () => {
     fetch(url)
